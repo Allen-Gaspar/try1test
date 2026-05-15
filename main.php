b23c96398492a1cc1da6c0c7f90c2985d0708235
Allen-Gaspar <len.10212005@gmail.com>
2026-05-15 21:59:32 +0800

    this is the main page
    careful lng

---
+++ main.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CessLen</title>
+    <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+        }
+        iframe {
+            width: 100%;
+            height: 100vh;
+            border: none;
+        }
+    </style>
+</head>
+<body>
+    <iframe src="topnav/nav.php" scrolling="yes"></iframe>
+</body>
+</html>
